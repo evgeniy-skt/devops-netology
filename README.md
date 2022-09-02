702cbc1326c70334f1ee7f61bf3a2127b995623a
“Evgeniy <e.ivanchenko@dodopizza.com>
2022-09-02 09:17:02 +0300

    Change file structure and add 6.3 homework

--- README.md
+++
@@ -1,7 +0,0 @@
-# devops-netology
-В дирректории `Terraform` будут проигнорированы
-- Все директории `.terraform` на любом уровне вложенности
-- Все файлы с расширением `.tfstate` (даже те, где эта строка будет встречаться не в конце имени файла)
-- Все файлы с расширением `.tfvars`
-- Файлы `override.tf`, `override.tf.json`, и все файлы с суффиксами `_override.tf`, `_override.tf.json`
-- Файлы `.terraformrc`, `terraform.rc`
--- cert_gene_script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-vault write -format=json pki_int/issue/example-dot-com common_name="example.com" ttl="720h" > /home/vagrant/example.com.crt
-cat /home/vagrant/example.com.crt | jq -r .data.certificate > /etc/nginx/sites-avaliable/example.com.crt.pem
-cat /home/vagrant/example.com.crt | jq -r .data.ca_chain[] >> /etc/nginx/sites-avaliable/example.com.crt.pem
-cat /home/vagrant/example.com.crt | jq -r .data.private_key > /etc/nginx/sites-avaliable/example.com.crt.key
