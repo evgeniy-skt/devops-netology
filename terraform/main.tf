6ebe2e0901d3f6c1269f903f716e997727cd2480
“Evgeniy <e.ivanchenko@dodopizza.com>
2022-09-15 09:30:54 +0300

    add 7.3 homework

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,13 @@ resource "yandex_compute_instance" "node01" {
   zone                      = "ru-central1-a"
   hostname                  = "node01.netology.cloud"
   allow_stopping_for_update = true
+  count = local.news_instance_count[terraform.workspace]
+  # instance_type = terraform.workspace == "prod"
+  #                 ? "m4.large"
+  #                 : "t2.micro"
+  lifecycle {
+    create_before_destroy = true
+  }
 
   resources {
     cores  = 2
@@ -37,8 +44,54 @@ resource "yandex_compute_instance" "node01" {
     subnet_id = yandex_vpc_subnet.subnet.id
     nat       = true
   }
+}
+
+  resource "yandex_compute_instance" "node02" {
+    name                      = "node02"
+    zone                      = "ru-central1-a"
+    hostname                  = "node02.netology.cloud"
+    for_each      = var.instances
+
+    resources {
+      cores  = 2
+      memory = 2
+    }
+
+    boot_disk {
+      initialize_params {
+        image_id    = data.yandex_compute_image.ubuntu.id
+        name        = "root-node02"
+        type        = "network-nvme"
+        size        = "50"
+      }
+    }
+
+    network_interface {
+      subnet_id = yandex_vpc_subnet.subnet.id
+      nat       = true
+    }
 
   metadata = {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+locals{
+  news_instance_count = {
+  default = 1
+  stage = 1
+  prod = 2
+}
+}
+
+variable "instances" {
+  type        = map
+  default     = {
+    node_1 = {
+      name                    = "foreach_node_1"
+    },
+    node_2 = {
+      name                    = "foreach_node_2"
+    }
   }
 }
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
-output "yandex_zone" {
-  value       = yandex_compute_instance.node01.zone
-}
-
-output "yandex_ip_private" {
-  value       = yandex_compute_instance.node01.network_interface.0.ip_address
-}
-
-output "yandex_vpc_subnet" {
-  value       = resource.yandex_vpc_subnet.subnet.id
-}
+# output "yandex_zone" {
+#   value       = yandex_compute_instance.node01[count.index].zone
+# }
+#
+# output "yandex_ip_private" {
+#   value       = yandex_compute_instance.node01[count.index].network_interface.0.ip_address
+# # }
+#
+# output "yandex_vpc_subnet" {
+#   value       = resource.yandex_vpc_subnet.subnet.id
+# }
